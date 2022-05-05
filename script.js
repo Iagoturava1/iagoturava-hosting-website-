f871c6ea2b5225237da14f6b224478dc6e22094f
Iagoturava1 <iago.turava25b@geolab.edu.ge>
2022-05-05 16:25:55 +0400

    x

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-let sliderImages = document.querySelectorAll('.slide'),
-    arrowRight = document.querySelector('#arrow-right'),
-    arrowLeft = document.querySelector('#arrow-left'),
-    i = 0
-
-
+let sliderImages = document.querySelectorAll(".slide"),
+  arrowRight = document.querySelector("#arrow-right"),
+  arrowLeft = document.querySelector("#arrow-left"),
+  i = 0;
 
 function reset() {
   for (let i = 0; i < sliderImages.length; i++) {
-    sliderImages[i].style.display = 'none'
+    sliderImages[i].style.display = "none";
   }
 }
 function startSlider() {
-  reset()
-  sliderImages[i].style.display = ''
+  reset();
+  sliderImages[i].style.display = "";
 }
-arrowRight.addEventListener('click', function() {
-  if(i > sliderImages.length - 2 ) {i = -1}
-  i++
-  startSlider()
-})
-arrowLeft.addEventListener('click', function() {
-  if (i === 0) {i = sliderImages.length}
-  i--
-  startSlider()
-})
+arrowRight.addEventListener("click", function () {
+  if (i > sliderImages.length - 2) {
+    i = -1;
+  }
+  i++;
+  startSlider();
+});
+arrowLeft.addEventListener("click", function () {
+  if (i === 0) {
+    i = sliderImages.length;
+  }
+  i--;
+  startSlider();
+});
 
 function alert1() {
-  let y = 1
-  alert(y)
-  y++
-  setTimeout(alert1, 6000)
+  let y = 1;
+  alert(y);
+  y++;
+  setTimeout(alert1, 6000);
 }
 // alert1()
 
-startSlider()
-
-
+startSlider();
 
 const hamburger = document.querySelector(".hamburger");
 const navmenu = document.querySelector(".nav-menu");
@@ -45,6 +45,4 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navmenu.classList.toggle("active");
   barr.classList.toggle("active");
-
-
-})
+});
